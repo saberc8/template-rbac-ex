573cfe3900f627c7136dd90f566cc9957c1d6a05
saber <1558365539@qq.com>
2026-02-12 01:23:30 +0800

    init

--- pc-admin-react/src/layouts/dashboard/header.tsx
+++ pc-admin-react/src/layouts/dashboard/header.tsx
@@ -6,7 +6,6 @@ import { cn } from "@/utils";
 import type { ReactNode } from "react";
 import AccountDropdown from "../components/account-dropdown";
 import BreadCrumb from "../components/bread-crumb";
-import NoticeButton from "../components/notice";
 import SearchBar from "../components/search-bar";
 import SettingButton from "../components/setting-button";
 
@@ -51,7 +50,6 @@ export default function Header({ leftSlot }: HeaderProps) {
 				>
 					<Icon icon="carbon:logo-discord" size={24} />
 				</Button>
-				<NoticeButton />
 				<SettingButton />
 				<AccountDropdown />
 			</div>
